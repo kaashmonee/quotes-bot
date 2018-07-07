d6e378c059706292a49a87c38c39eff3d6f35cdc
skanda <skandakk@gmail.com>
2018-07-07 18:43:01 -0400

    Obtaining posts functional

--- bot.py
+++ bot.py
@@ -2,7 +2,12 @@ import praw
 
 
 def main():
-    reddit = praw.Reddit("")
+    reddit = praw.Reddit("bot1")
+    subreddit = reddit.subreddit("quotesporn")
+
+    # Getting top 5 hot posts
+    for submission in subreddit.hot(limit=5):
+        print("submission:", submission)
 
 if __name__ == "__main__":
     main()
