d864f0f5ebe8292dfb54e61c48b186ba8aca6d92
skanda <skandakk@gmail.com>
2018-07-07 18:55:15 -0400

    Added code skeleton

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import praw
+import pprint
 
 
 def main():
@@ -7,7 +8,19 @@ def main():
 
     # Getting top 5 hot posts
     for submission in subreddit.hot(limit=5):
-        print("submission:", submission)
+        pprint.pprint(vars(submission))
+        image_url = submission["url"]
+        ocr_text = do_ocr(image_url)
+        comment_on_post(ocr_text)
+
+        break
+
+
+def do_OCR(image_url) -> str:
+    pass
+
+def comment_on_post(ocr_text):
+    pass
 
 if __name__ == "__main__":
     main()
