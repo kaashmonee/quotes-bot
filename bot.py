6457f0bc60ee8dd6eeb17e4c5918595ee57b5170
skanda <skandakk@gmail.com>
2018-07-07 19:03:56 -0400

    Refactored for OOP

--- bot.py
+++ bot.py
@@ -1,26 +1,31 @@
 import praw
 import pprint
 
+class OCRBot:
+
+    def __init__(self):
+        self.reddit = praw.Reddit("bot1")
+        self.subreddit = self.reddit.subreddit("quotesporn")
+
+    def run_bot(self):
+        # Getting top 5 hot posts
+        for submission in self.subreddit.hot(limit=5):
+            image_url = submission.url
+            ocr_text = self.do_OCR(image_url)
+            self.comment_on_post(ocr_text)
+            print("image url:", image_url)
+
+            break
+
+    def do_OCR(self, image_url):
+        pass
+    
+    def comment_on_post(self, ocr_text):
+        pass
 
 def main():
-    reddit = praw.Reddit("bot1")
-    subreddit = reddit.subreddit("quotesporn")
-
-    # Getting top 5 hot posts
-    for submission in subreddit.hot(limit=5):
-        pprint.pprint(vars(submission))
-        image_url = submission["url"]
-        ocr_text = do_ocr(image_url)
-        comment_on_post(ocr_text)
-
-        break
-
-
-def do_OCR(image_url) -> str:
-    pass
-
-def comment_on_post(ocr_text):
-    pass
+    bot = OCRBot()
+    bot.run_bot()
 
 if __name__ == "__main__":
     main()
